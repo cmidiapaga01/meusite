137941c966a3cc650438a67c778c8f4f8883d236
cmidiapaga01 <124449911+cmidiapaga01@users.noreply.github.com>
2024-04-02 22:33:42 +0200

    ma oe

--- src/components/Tag1.js
+++ src/components/Tag1.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import { Box, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, useDisclosure, Flex, Text, Spacer, Input, Textarea } from '@chakra-ui/react';
-import { InfoIcon, InfoOutlineIcon, CheckCircleIcon, WarningIcon, QuestionIcon } from '@chakra-ui/icons'; // Importando ícones do Chakra UI
 import StatusIndicator from './StatusIndicator'; // Importando o componente StatusIndicator
 
 // Importando imagens
-import image1 from '../images/code.svg';
+import image1 from '../images/cat_no_bg.png';
+import checkCircleIcon from '../images/dns.svg'; // Importe a imagem do ícone de círculo de verificação
+import questionIcon from '../images/md.svg'; // Importe a imagem do ícone de círculo de interrogação
 
 const Tag1 = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [currentTag, setCurrentTag] = useState(null);
-  const [buttonText, setButtonText] = useState('Open Modal 1');
+  const [buttonText, setButtonText] = useState('Open Modal');
   const [showLoadedText, setShowLoadedText] = useState(false);
 
   const openModal = (tag) => {
@@ -30,7 +31,7 @@ const Tag1 = () => {
   const tag = { imageSrc: image1, description: 'GTM tag was fired bitch!!!!!', category: 'Analytics' };
 
   return (
-    <Box backgroundColor = "orange" id="tagSection">
+    <Box id="tagSection">
       <Box key="tag1" maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" boxShadow="md" m="2">
         <Box textAlign="center">
           <img src={tag.imageSrc} alt="Tag Image" />
@@ -50,10 +51,10 @@ const Tag1 = () => {
         <ModalContent>
           <ModalHeader>
             <Flex align="center">
-              <Box mr={2}><InfoIcon boxSize={6} color="blue.500" /></Box>
+              <Box mr={2}><img src={checkCircleIcon} alt="Check Circle" /></Box> {/* Usando a imagem do ícone de círculo de verificação */}
               <Text fontWeight="bold">{currentTag?.category}</Text>
               <Spacer />
-              <Box mr={2}><InfoOutlineIcon boxSize={6} color="blue.500" /></Box>
+              <Box mr={2}><img src={questionIcon} alt="Question Circle" /></Box> {/* Usando a imagem do ícone de círculo de interrogação */}
               <Text>{currentTag?.description}</Text>
             </Flex>
           </ModalHeader>
@@ -66,17 +67,17 @@ const Tag1 = () => {
             <Flex flexDirection="column" mt="4" alignItems="start">
               <Box>
                 <Flex alignItems="center">
-                  <Box mr={2}><CheckCircleIcon color="green.500" /></Box>
-                  <Text fontWeight="bold" color="green.500">Sucesso:</Text> {/* Mudando a cor do texto para verde */}
+                  <Box mr={2}><img src={checkCircleIcon} alt="Check Circle" /></Box> {/* Usando a imagem do ícone de círculo de verificação */}
+                  <Text fontWeight="bold" color="green.500">Sucesso:</Text>
                 </Flex>
-                <Text ml="8" color="green.500">A tag foi disparada com sucesso e está rastreando eventos no site.</Text> {/* Mudando a cor do texto para verde */}
+                <Text ml="8" color="green.500">A tag foi disparada com sucesso e está rastreando eventos no site.</Text>
               </Box>
               <Box mt="4">
                 <Flex alignItems="center">
-                  <Box mr={2}><QuestionIcon color="gray.500" /></Box>
-                  <Text fontWeight="bold" color="gray.500">Informação adicional:</Text> {/* Mudando a cor do texto para cinza */}
+                  <Box mr={2}><img src={questionIcon} alt="Question Circle" /></Box> {/* Usando a imagem do ícone de círculo de interrogação */}
+                  <Text fontWeight="bold" color="gray.500">Informação adicional:</Text>
                 </Flex>
-                <Text ml="8" color="gray.500">Você pode integrar esta tag com outras ferramentas de análise para obter mais insights.</Text> {/* Mudando a cor do texto para cinza */}
+                <Text ml="8" color="gray.500">Você pode integrar esta tag com outras ferramentas de análise para obter mais insights.</Text>
               </Box>
             </Flex>
           </ModalBody>
