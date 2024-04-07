c4b645fa455feb1a453c50cdc2bb213ae4e5e856
i-Moe <potato3gg@gmail.com>
2024-04-07 13:55:11 +0300

    let me commit thank you

--- src/components/Tag/TagLoadButton.js
+++ src/components/Tag/TagLoadButton.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { Button } from '@chakra-ui/react';
-import { animated } from '@react-spring/web';
-import styles from '../styles.module.css';
+import React from "react";
+import { Button, Box } from "@chakra-ui/react";
+import { animated } from "@react-spring/web";
+import styles from "../styles.module.css";
 
 const TagLoadButton = ({ onClick, props }) => {
-  
   return (
-    <Button onClick={onClick} className={styles.main}>
-      <animated.div className={styles.fill} style={props} />
-      <animated.div className={styles.content}>
-        {props.width.to((x) =>
-          x === 0 ? 'Start' : x === 200 ? 'Finish' : ''
-        )}
-      </animated.div>
-    </Button>
+    <Box p="4" className={styles.container}>
+      <Button onClick={onClick} className={styles.main}>
+        <animated.div className={styles.fill} style={props} />
+        <animated.div className={styles.content}>
+          {props.width.to((x) =>
+            x === 0 ? "Start" : x === 200 ? "Finish" : ""
+          )}
+        </animated.div>
+      </Button>
+    </Box>
   );
 };
 
 export default TagLoadButton;
-
 
 // import React, { useState } from "react";
 // import { Button, Box } from "@chakra-ui/react";
